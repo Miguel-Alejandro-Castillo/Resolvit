fb19f6ce1418f54e12518b2ab9afd8e1022abbc3
CM118821 <miguel.castillofigueroa@icbc.com.ar>
2019-11-27 17:38:07 -0300

    clear logic

--- src/Solution2.java
+++ src/Solution2.java
@@ -1,33 +1,25 @@
-import java.util.HashSet;
-import java.util.Random;
-import java.util.Set;
+import java.util.*;
 
 public class Solution2 {
 
     public int[] solution(int N) {
-        Set<Integer> uniques = new HashSet<Integer>();
+        Set<Integer> uniques = new HashSet<>();
         if(N > 0) {
-            if(N == 1) {
-                uniques.add(0);
+            Random random = new Random();
+            while (uniques.size() < N - 1) {
+                int number = random.nextInt();
+                uniques.add(number);
             }
-            else {
-                Random random = new Random();
-                while (uniques.size() < N - 1) {
-                    int number = random.nextInt();
-                    uniques.add(number);
-                }
-                int resto = uniques.stream().reduce(0, Integer::sum) * -1;
-                uniques.add(resto);
-            }
+            int resto = uniques.stream().reduce(0, Integer::sum) * -1;
+            uniques.add(resto);
         }
 
-        Integer []integers = (Integer[]) uniques.toArray();
+        Object []integers =  uniques.toArray();
         int []result = new int[integers.length];
         for(int i = 0; i < integers.length; i++) {
-            result[i] = integers[i];
+            result[i] = (int)integers[i];
         }
 
         return result;
     }
-
 }
